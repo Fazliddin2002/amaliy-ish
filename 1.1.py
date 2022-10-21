4c606fa784fe64fc25e39e0630f1e1af8d3d4a88
Fazliddin <116306698+Fazliddin2002@users.noreply.github.com>
2022-10-21 09:01:35 +0500

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,4 @@
+a=int(input("1kg mahsulot narxini kiriting: "))
+b=int(input("mahsulot o'g'irligini kiriting:(gramda) "))
+s=b/1000*a
+print(f'mahsulotning narxi={s} som')
---
+++ 1.2.py
@@ -0,0 +1,4 @@
+a=int(input("1kg mahsulot narxini somda kiriting: "))
+b=int(input("mahsulot o'g'irligini kiriting:(gramda) "))
+s=b/1000*a
+print(f'mahsulotning narxi={s} som')
